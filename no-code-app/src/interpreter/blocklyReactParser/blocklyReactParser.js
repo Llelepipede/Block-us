3fc3aba23c228954dda3f428d3e65fbfbafcc3bd
Llelepipede <pchesneau3103@gmail.com>
2024-08-30 11:13:35 +0200

    automatic div around stand alone block

--- no-code-app/src/interpreter/blocklyReactParser/blocklyReactParser.js
+++ no-code-app/src/interpreter/blocklyReactParser/blocklyReactParser.js
@@ -3,6 +3,7 @@ import { debug } from "./debug";
 import Workspace from "../../components/Workspace";
 import Button from "../../components/createdComponents/Button";
 import Text from "../../components/createdComponents/Text";
+import Initializer from './initializer';
 
 
 // ici y'auras tout le coeur du parser/interpreter, chaque block aura sont output précisé ici meme
@@ -11,21 +12,30 @@ export function blocklyReactParser(workspaceJson,actualWorkspace)  {
   var content = [Workspace]
     if (workspaceJson && workspaceJson.blocks && workspaceJson.blocks.blocks){
         workspaceJson.blocks.blocks.forEach((block, index) => {
-            debug(block,index)  
+            let parent = Initializer();
+            let children = Initializer();
+            // debug(block,index)  
+            console.log("parent",parent)
             if (block.type === "react_button"){
               console.log("button");
               // console.log(block.fields["BUTTON_TEXT"]);
-              content = [...content,  <Button text={block.fields["BUTTON_TEXT"]} onClick={null} />];
+              children = React.cloneElement(parent.props.children,null,<Button text={block.fields["BUTTON_TEXT"]} onClick={null} />)
+              // content = [...content,  <Button text={block.fields["BUTTON_TEXT"]} onClick={null} />];
             }
             if (block.type === "react_text"){
               console.log("text");
-              content = [...content,  <Text text={block.fields["TEXT_TEXT"]} />];
+              children = React.cloneElement(parent.props.children,null,<Text text={block.fields["TEXT_TEXT"]} />)
+              // content = [...content,  <Text text={block.fields["TEXT_TEXT"]} />];
             }
+            console.log("children",children);
+            let final = React.cloneElement(parent,children.props)
+            console.log("final",final)
+            content = [...content,React.cloneElement(parent,children.props)];
           });
     }
+    console.log("content end",content);
     console.log("prout",actualWorkspace)
     const newChildren = React.cloneElement(actualWorkspace.props.children,...content)
     console.log(newChildren);
     return React.cloneElement(actualWorkspace,newChildren.props);
 }
-
---
+++ no-code-app/src/interpreter/blocklyReactParser/initializer.js
@@ -0,0 +1,15 @@
+// src/Button.js
+import React from 'react';
+
+const Initializer = () => {
+  return (
+    <div >
+        <div >
+        </div>
+        <div >
+        </div>
+    </div>
+  );
+};
+
+export default Initializer;
